3ed8df27ef15ea1d4e8d038debe33a1144ed3fb0
Usman Khan <mdusmanhanifkhan@gmail.com>
2026-03-25 20:30:57 +0500

    update the server.js

--- server.js
+++ server.js
@@ -6,23 +6,23 @@ import routes from "./routes/index.js"
 import cors from "cors"
 const app = express()
 
-const port = process.env.PORT || 3003;
-const FRONTEND_SERVICE_ORIGIN = process.env.CORE_ORIGIN_FRONTEND; 
-const APP_BASE_URL = process.env.APP_BASE_URL;
-const APP_BASE_URL_WWW = process.env.APP_BASE_URL_WWW;
+const port = process.env.PORT || 3000;
+// const FRONTEND_SERVICE_ORIGIN = process.env.CORE_ORIGIN_FRONTEND; 
+// const APP_BASE_URL = process.env.APP_BASE_URL;
+// const APP_BASE_URL_WWW = process.env.APP_BASE_URL_WWW;
 
-const allowedOrigins = [
-  "https://hikarimed.online",
-  "https://www.hikarimed.online",
-  "https://hikarimed.vercel.app",
-  "http://localhost:5173",
-  "https://md2vt317-5173.inc1.devtunnels.ms",
-  "https://hikarimed-v2.vercel.app"
-];
+// const allowedOrigins = [
+//   "https://hikarimed.online",
+//   "https://www.hikarimed.online",
+//   "https://hikarimed.vercel.app",
+//   "http://localhost:5173",
+//   "https://md2vt317-5173.inc1.devtunnels.ms",
+//   "https://hikarimed-v2.vercel.app"
+// ];
 
 
 app.use(cors({
-  origin: allowedOrigins,
+  origin: '*',
   credentials: true, 
 }))
 
